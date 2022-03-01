2dd1dc39b7f9c2f96fcce75d16228b6d71c212cf
Victor Silva <victor.silvaPC@hotmail.com>
2022-02-28 23:20:40 -0300

    feat: using cors library to allow access of external entities

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,18 @@ import { courseRoutes } from "./routes/courses.routes";
 import { studentRoutes } from "./routes/students.routes";
 import { teacherRoutes } from "./routes/teachers.routes";
 
+import cors from "cors";
+
 const app = express();
+
+app.use((req, res, next) => {
+  //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  //Quais são os métodos que a conexão pode realizar na API
+  res.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  app.use(cors());
+  next();
+});
 
 app.use(express.json());
 
