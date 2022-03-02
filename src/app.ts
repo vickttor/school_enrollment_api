023da994a0a16ecef8de2feeb8723a528dff4d0a
Victor Silva <victor.silvaPC@hotmail.com>
2022-03-02 15:54:04 -0300

    fix: url origin changed

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,10 @@ const app = express();
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header("Access-Control-Allow-Origin", " http://localhost:3000 ");
+  res.header(
+    "Access-Control-Allow-Origin",
+    " https://school-enrollment-ax4b.vercel.app/ "
+  );
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Request-Width, Content-Type, Accept"
